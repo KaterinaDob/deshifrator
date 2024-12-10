70dc14c1054109c53cc32b53fb3a70e5d514bf0c
Ekaterina Dobler <catherinedobler@yandex.ru>
2024-12-10 22:28:37 +0300

    first_try1

--- decoder.py
+++ decoder.py
@@ -7,19 +7,15 @@ def decode_string(s: str) -> str:
 
     for char in s:
         if char.isdigit():
-            # Получаем число
             current_num = current_num * 10 + int(char)
         elif char == '[':
-            # Сохраняем текущую строку и число в стек
             stack.append((current_str, current_num))
             current_str = ''
             current_num = 0
         elif char == ']':
-            # Извлекаем строку и число из стека
             last_str, num = stack.pop()
             current_str = last_str + current_str * num
         else:
-            # Просто добавляем символ к текущей строке
             current_str += char
 
     return current_str
